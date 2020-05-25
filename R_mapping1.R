5d2404a0275ec49a6f402d11b06772052d9a4bd1
turingskj <turingskj@hotmail.com>
2020-05-25 06:32:12 -0400

    new comit for mapping

--- R_mapping1.R
+++ R_mapping1.R
@@ -1,15 +1,15 @@
-library(sf)
-library(raster)
-library(dplyr)
-library(spData)
-library(spDataLarge)
+#library(sf)
+#library(raster)
+#library(dplyr)
+#library(spData)
+#library(spDataLarge)
 
 library(tmap)    # for static and interactive maps
 #library(leaflet) # for interactive maps
-library(mapview)
+#library(mapview)
 library(ggplot2)
 
-library(shiny)
+#library(shiny)
 
 tmap_mode("plot")
 
@@ -21,5 +21,15 @@ us_states_map
 library(usmap)
 mdcountypop<-usmap::countypop[which(usmap::countypop$abbr=="MD"),]
 
-plot_usmap("counties", include=c("MD"), data= mdcountypop, value="pop_2015") +  
-  scale_fill_continuous(low = "white", high = "red", name = "Population (2015)", label = scales::comma)
+fileconnect1 <- url("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
+Alldata <- read.table(fileconnect1, sep =",", header = TRUE, encoding="UTF-8", quote = "\"")
+# or
+Alldata <- read.csv(fileconnect1, sep =",", header = TRUE, encoding="UTF-8")
+
+head(Alldata)
+
+mddata <- subset(Alldata, state=="Maryland" & date =="2020-05-23")
+
+
+plot_usmap("counties", include=c("MD"), data= mddata, value="cases") +  
+  scale_fill_continuous(low = "white", high = "red", name = "MD Covid-19 cases", label = scales::comma)
