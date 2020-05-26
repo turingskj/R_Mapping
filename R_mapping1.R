184dab42fea5df8f85936da81a8082ca3d795e53
turingskj <turingskj@hotmail.com>
2020-05-26 04:11:55 -0400

    data update

--- R_mapping1.R
+++ R_mapping1.R
@@ -1,8 +1,8 @@
 #library(sf)
 #library(raster)
 #library(dplyr)
-#library(spData)
-#library(spDataLarge)
+library(spData)
+library(spDataLarge)
 
 library(tmap)    # for static and interactive maps
 #library(leaflet) # for interactive maps
@@ -11,11 +11,21 @@ library(ggplot2)
 
 #library(shiny)
 
-tmap_mode("plot")
+# dev.cur() # shows the current graphic devices
+# dev.off() # delete current output graphic device
+# png("filename.png") or jpeg(...) or windows()... redirect the graphic output
+# pdf(...)
+# dev.off() will clear the graphic output device
+
+
+# using tmap library
+
+tmap_mode(c("plot", "view"))
 
 us_states_map <- tm_shape(us_states, projection = 2163) + tm_polygons() + 
   tm_layout(frame = FALSE)
 us_states_map
+
 
 
 library(usmap)
@@ -24,12 +34,25 @@ mdcountypop<-usmap::countypop[which(usmap::countypop$abbr=="MD"),]
 fileconnect1 <- url("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
 Alldata <- read.table(fileconnect1, sep =",", header = TRUE, encoding="UTF-8", quote = "\"")
 # or
-Alldata <- read.csv(fileconnect1, sep =",", header = TRUE, encoding="UTF-8")
+# Alldata <- read.csv(fileconnect1, sep =",", header = TRUE, encoding="UTF-8")
 
 head(Alldata)
+namelength <- sapply(as.character(Alldata$county), nchar) # find the lenght of county name
+longestName <- which.max(namelength) # give a integer value with a name (a longest county name)
+names(longestName)
 
-mddata <- subset(Alldata, state=="Maryland" & date =="2020-05-23")
+# library(readr)
+# tt<-parse_number(countynames)
+
+#library(tidyr)
+#tempnumeric <- extract_numeric(countynames) # extrac numbers from string-numeric mix. If no numeric value, "NA"
+#temp1 <- as.numeric(is.na(tempnumeric)) # temp1 will be ones if all NAs in tempnueric (no numeric in county names)
+#temp1 <- sum(temp1) 
+# if nrow(Alldata) is the same as temp1, then no county names have numeric value
+
+mydate <- "2020-05-24"
+mddata <- subset(Alldata, state=="Maryland" & date ==mydate)
 
 
 plot_usmap("counties", include=c("MD"), data= mddata, value="cases") +  
-  scale_fill_continuous(low = "white", high = "red", name = "MD Covid-19 cases", label = scales::comma)
+  scale_fill_continuous(low = "white", high = "red", name = paste("MD Covid-19 cases:", mydate), label = scales::comma)
