15c95225af2317d27691f0035ced0a591801114d
turingskj <turingskj@hotmail.com>
2020-05-27 01:43:20 -0400

    usmap package ussage update

--- R_mapping1.R
+++ R_mapping1.R
@@ -27,19 +27,29 @@ us_states_map <- tm_shape(us_states, projection = 2163) + tm_polygons() +
   tm_layout(frame = FALSE)
 us_states_map
 
-
 # adding user data for plot. Note that the data just needs to be match the number of category
 
 my_usstates <- us_states[c("GEOID", "NAME")]
 visited <- runif(nrow(my_usstates), min=0, max=20)
 my_usstates$visited = floor(visited+0.5)
-tm_shape(my_usstates) + tm_polygons("visited")
+tm_shape(my_usstates, projection = 2163) + tm_polygons("visited")
+tm_shape(my_usstates, projection = 2163) + tm_polygons()
+
+
+# rgdal package --> download the polygon data from http://www.personal.psu.edu/users/a/c/acr181/election.html
+library(GWmodel)
+library(rgdal)
+US3 <- readOGR(dsn="2004_Election", layer = "2004_Election_Counties")
+plot(US3)
+
+
 
 
 
 # usmap package
 
 library(usmap)
+library(ggplot)
 mdcountypop<-usmap::countypop[which(usmap::countypop$abbr=="MD"),]
 
 fileconnect1 <- url("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
@@ -63,9 +73,27 @@ names(longestName)
 
 mydate <- "2020-05-24"
 mddata <- subset(Alldata, state=="Maryland" & date ==mydate)
+dedata <- subset(Alldata, state=="Delaware" & date ==mydate)
+
+plot_usmap(include=c("MD"), data= mddata, value="cases") +  
+  scale_fill_continuous(low = "white", high = "red", name = paste("MD Covid-19 cases:", mydate), label = scales::comma, 
+                        limit = c(0, 20000)) +  theme(legend.position = "right" ) 
 
 
-plot_usmap("counties", include=c("MD"), data= mddata, value="cases") +  
-  scale_fill_continuous(low = "white", high = "red", name = paste("MD Covid-19 cases:", mydate), label = scales::comma)
+plot_usmap(data=statepop, value="pop_2015") +
+  scale_fill_continuous(low = "white", high = "red", name = paste("2015 State Pop", mydate), label = scales::comma) +
+  theme(legend.position = "right" )
+
+plot_usmap(data=countypop, value="pop_2015") +
+  scale_fill_continuous(low = "white", high = "red", name = paste("2015 County Pop", mydate), label = scales::comma, 
+                        trans = "log")
 
 
+# two different maps with two different colors
+mdstate <- plot_usmap(include=c("MD"), data= mddata, value="cases", color="black", size=1.5)  
+
+destate <- plot_usmap(include=c("DE"), data= dedata, value="cases", color="blue", size =1.5)  
+
+ggplot() + mdstate$layers[[1]] + destate$layers[[1]] + mdstate$theme +  
+  scale_fill_continuous(low = "white", high = "red", name = paste("Covid-19 cases:", mydate), label = scales::comma, 
+                        limit = c(0, 15000)) +  theme(legend.position = "right" )
